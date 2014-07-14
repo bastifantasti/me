ffe3e7a20f0f9c8f5e67b652bf1ed4d6938d0516
Sebastian Felix Schwarz <sebastian.schwarz@pixelpark.com>
2014-07-14 21:34:26 +0200

    pfade geändert

--- app/scripts/projekt.js
+++ app/scripts/projekt.js
@@ -1,24 +1,15 @@
 // Mixitup Filter Gallery
 jQuery(document).ready(function ($) {
-    $(function(){
-        $('.work-grid').mixitup();
-    });
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        arrows: true,
+        useCSS: true
 
-// Swipebox Gallery
-    $(function($) {
-        $(".swipebox").swipebox();
-    });
-
-// Nivo Slider
-    $(window).load(function() {
-        $('.slider').nivoSlider({
-            effect: 'boxRandom',
-            slices: 15,
-            boxCols: 8,
-            boxRows: 4,
-            animSpeed: 500,
-            pauseTime: 3000,
-            controlNav: true
-        });
     });
 });
